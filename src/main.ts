90870ff5dc91b8a18a15029c4537d6f6e34d5412
Vilson C Monteiro <vcmorion@hotmail.com>
2025-11-29 08:12:55 -0300

    validação produtos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    //ativa validação global
+    new ValidationPipe({
+      whitelist: true, //remove propriedades não definidas no DTO
+      forbidNonWhitelisted: true, //gera erro se propriedades não definidas no DTO forem enviadas
+      transform: true, //converte tipos automaticamente com base nos tipos definidos no DTO
+    }),
+  );
+  await app.listen(3000);
 }
 bootstrap();
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import { IsString, IsNumber } from 'class-validator';
+import { IsString, IsNumber, Length, Min } from 'class-validator';
 
 export class CreateProductDto {
   @IsString()
+  @Length(3, 50)
   name: string;
 
   @IsNumber()
+  @Min(0)
   price: number;
 }
