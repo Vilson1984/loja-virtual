fff8241314286a393e71ea56e4b0d294734b42f3
Vilson C Monteiro <vcmorion@hotmail.com>
2025-11-30 19:52:47 -0300

    CRUD Category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { createCategoryDto } from './entities/dto/create-category.dto';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Post()
+  criarCategoria(@Body() data: createCategoryDto) {
+    console.log('Está no Controller', data);
+    return this.categoryService.criarCategoria(data);
+  }
+
+  @Get()
+  getCategories() {
+    console.log('ESTÁ NO CONTROLLER - GET CATEGORY');
+    return this.categoryService.getCategories();
+  }
+
+  @Patch(':id')
+  updateCategory(@Param('id') id: string, @Body() dto: createCategoryDto) {
+    console.log('ESTÁ NO CONTROLLER PATCH CATEGORY', id);
+    return this.categoryService.updateCategory(+id, dto); // +id converte string para number
+  }
+
+  @Delete(':id')
+  deleteCategory(@Param('id') id: string) {
+    console.log('ESTÁ NO CONTROLLER DELETE CATEGORY', id);
+    return this.categoryService.deleteCategory(+id);
+  }
+}
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Repository } from 'typeorm';
+import { createCategoryDto } from './entities/dto/create-category.dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
+  ) {}
+
+  criarCategoria(data: createCategoryDto) {
+    const novaCategoria = this.categoryRepository.create(data);
+    console.log('Nova Categoria Criada:', novaCategoria);
+    return this.categoryRepository.save(novaCategoria);
+  }
+
+  getCategories() {
+    const categories = this.categoryRepository.find();
+    console.log('CATEGORIES SERVICE:', categories);
+    return categories;
+  }
+
+  async updateCategory(id: number, dto: createCategoryDto) {
+    const category = await this.categoryRepository.findOneBy({ id });
+    console.log('UPDATE CATEGORY SERVICE:', category);
+
+    if (!category) {
+      throw new Error('Category not found');
+    }
+
+    //Mesclaa os dados recebidos de dto com a categoria passada por id
+    Object.assign(category, dto);
+
+    return this.categoryRepository.save(category);
+  }
+
+  deleteCategory(id: number) {
+    console.log('DELETE CATEGORY SERVICE:', id);
+    return this.categoryRepository.delete(id);
+  }
+}
